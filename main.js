35a9dc14eaab1110489dfd0f229758e868069b2d
KierstonHill83 <khill1218@gmail.com>
2015-07-10 23:21:41 -0600

    Added a blank html and added javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+for(i = 0; i < 1001; i += 100) {
+  console.log(i);
+}
+
+for(i = 0; i < 8; i++) {
+  console.log(Math.pow(2, i));
+}
+
+for(i = 0; i < 11; i += 2) {
+  console.log(i);
+}
+
+for(i = 3; i < 16; i += 3) {
+  console.log(i);
+}
+
+for(i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+for(i = 1; i < 5; i++) {
+  for(n = 0; n < 3; n++) {
+    console.log(i);
+  }
+}
+
+for(i = 0; i < 15; i++) {
+  console.log(i % 5);
+}
